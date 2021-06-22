708e7b4bd59f452b6c1978fbf006b1dac92dadb4
Simon Jornet <jornetsimon@gmail.com>
2021-06-22 11:31:51 +0200

    pwa update snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'cb-root',
@@ -6,4 +8,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	styleUrls: ['./app.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor(update: SwUpdate, snackbar: MatSnackBar) {
+		update.available.subscribe((upd: any) => {
+			const snack = snackbar.open('Une mise à jour est disponible', `C'est parti !`, {
+				horizontalPosition: 'center',
+				panelClass: 'updateSnack',
+			});
+			snack.onAction().subscribe((value) => window.location.reload());
+		});
+	}
+}
