284ce2c9886b4c2be602a88cec80dbe19213c9ad
Simon Jornet <jornetsimon@gmail.com>
2021-08-19 13:44:17 +0200

    fix(auth): signInWithRedirect on iOS for installed PWA

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 import { HotToastService } from '@ngneat/hot-toast';
+import UserCredential = firebase.auth.UserCredential;
 
 @Component({
 	selector: 'cb-login',
@@ -17,18 +18,30 @@ export class LoginComponent {
 	) {}
 
 	login() {
-		this.angularFireAuth
-			.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-			.then((credential) => {
-				if (credential.user) {
-					this.toastService.success(
-						`Connecté en tant que ${credential.user.displayName}`,
-						{
-							duration: 3000,
-						}
-					);
-					this.router.navigateByUrl('/');
-				}
+		const navigator: any = window.navigator;
+		const authProvider = new firebase.auth.GoogleAuthProvider();
+		if ('standalone' in navigator && navigator.standalone) {
+			// Sign in with redirect on iOS installed PWA
+			this.angularFireAuth.signInWithRedirect(authProvider).then(() => {
+				this.toastService.success(`Connecté`, {
+					duration: 3000,
+				});
+				this.router.navigateByUrl('/');
 			});
+		} else {
+			this.angularFireAuth
+				.signInWithPopup(authProvider)
+				.then((credential: UserCredential) => {
+					if (credential.user) {
+						this.toastService.success(
+							`Connecté en tant que ${credential.user.displayName}`,
+							{
+								duration: 3000,
+							}
+						);
+						this.router.navigateByUrl('/');
+					}
+				});
+		}
 	}
 }
