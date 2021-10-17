baa20bed67bcc741c0d30111597b8c74e3be1eb9
Simon Jornet <jornetsimon@gmail.com>
2021-10-17 15:34:56 +0200

    update weather every hour

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AppQuery } from '../../state/app.query';
-import { filter, map, switchMap, take } from 'rxjs/operators';
+import { filter, map, shareReplay, switchMap, take, tap } from 'rxjs/operators';
 import { CityWeather } from './model/cityWeather';
+import { timer } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,17 +11,25 @@ import { CityWeather } from './model/cityWeather';
 export class WeatherService {
 	constructor(private http: HttpClient, private appQuery: AppQuery) {}
 
-	private API_KEY = '633570e4f43519c6abcb3649cabce6c4';
+	private API_KEY = '633570e4f43519c6abcb3649cabce6c4'; // steal that if you want 🤷
+
+	private everyHour$ = timer(0, 60 * 60 * 1000);
 
 	weather$ = this.appQuery.select('userData').pipe(
 		map((user) => user?.config.city),
 		filter(Boolean),
 		take(1),
 		switchMap((city) =>
-			this.http.get<CityWeather>(
-				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=metric&lang=fr`
+			this.everyHour$.pipe(
+				switchMap(() =>
+					this.http.get<CityWeather>(
+						`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=metric&lang=fr`
+					)
+				),
+				tap(() => console.log('Updated weather data ⛅️'))
 			)
-		)
+		),
+		shareReplay({ bufferSize: 1, refCount: true })
 	);
 
 	weatherIconUrl$ = this.weather$.pipe(
