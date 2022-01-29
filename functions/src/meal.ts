307f61c269e0efd2ee97337ae8e6f0418b37debb
Simon Jornet <jornetsimon@gmail.com>
2022-01-29 12:41:21 +0100

    most used names

--- functions/src/meal.ts
+++ functions/src/meal.ts
@@ -4,21 +4,73 @@ import { Dish, Meal, Recipe } from './model/meal.model';
 import { PickByType } from './helpers/pick-by-type';
 import { RecursiveKeyOf } from './helpers/recursive-key-of';
 import { normalizeString } from './helpers/normalize-string';
+import { firestore } from 'firebase-admin';
+import { isNotNullOrUndefined } from './helpers/is-not-null-or-undefined.helper';
+import slugify from 'slugify';
 import get = require('lodash/get');
 import uniq = require('lodash/uniq');
+import FieldValue = firestore.FieldValue;
 
 export const createMeal = functions
 	.region('europe-west1')
 	.firestore.document('users/{userId}/meals/{mealId}')
-	.onWrite((change, context) => {
-		if (!change.after.exists) {
-			// The document has been deleted
-			return;
+	.onWrite(async (change, context) => {
+		const mostUsedRef = db.collection(`users/${context.params.userId}/most-used`);
+		const mealBefore = change.before.exists ? (change.before.data() as Meal) : undefined;
+		const mealAfter = change.after.exists ? (change.after.data() as Meal) : undefined;
+		const removedNames: string[] = mealBefore ? getMealCustomNames(mealBefore) : [];
+		const addedNames: string[] = mealAfter ? getMealCustomNames(mealAfter) : [];
+
+		const batch = db.batch();
+
+		removedNames
+			.filter((name) => !addedNames.includes(name))
+			.forEach((name) => {
+				batch.set(
+					mostUsedRef.doc(slugify(name, { lower: true })),
+					{
+						name,
+						count: FieldValue.increment(-1),
+					},
+					{
+						merge: true,
+					}
+				);
+			});
+		addedNames
+			.filter((name) => !removedNames.includes(name))
+			.forEach((name) => {
+				batch.set(
+					mostUsedRef.doc(slugify(name, { lower: true })),
+					{
+						name,
+						count: FieldValue.increment(1),
+					},
+					{
+						merge: true,
+					}
+				);
+			});
+
+		await batch.commit();
+
+		if (mealAfter) {
+			// The document has not been created or updated
+			const searchKeys = generateMealSearchKeys(mealAfter);
+			db.doc(change.after.ref.path).update({ searchKeys });
 		}
-		const meal = change.after.data() as Meal;
-		const searchKeys = generateMealSearchKeys(meal);
-		db.doc(change.after.ref.path).update({ searchKeys });
 	});
+
+function getMealCustomNames(meal: Meal): string[] {
+	const primaryMealName = meal.name === meal.jowRecipe?.title ? undefined : meal.name;
+	const secondaryMealName =
+		meal.alternateDish?.name === meal.alternateDish?.jowRecipe?.title
+			? undefined
+			: meal.alternateDish?.name;
+	return [primaryMealName, secondaryMealName]
+		.map((name) => name?.trim() || undefined)
+		.filter(isNotNullOrUndefined);
+}
 
 function generateMealSearchKeys(meal: Meal): Meal['searchKeys'] {
 	const keys: Array<AllowedSearchKey> = [
