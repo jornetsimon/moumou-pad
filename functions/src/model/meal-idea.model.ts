9554abc5f35abf009242e2ce50d55748f1038aab
Simon Jornet <jornetsimon@gmail.com>
2024-06-08 17:38:46 +0200

    more MealIdea properties

--- functions/src/model/meal-idea.model.ts
+++ functions/src/model/meal-idea.model.ts
@@ -2,4 +2,8 @@ export interface MealIdea {
 	name: string;
 	image?: string;
 	tags: string[];
+	url?: string;
+	rating: number;
+	notionPageUrl: string;
+	created_at: string;
 }
--- functions/src/notion-ideas.ts
+++ functions/src/notion-ideas.ts
@@ -1,29 +1,33 @@
 import * as functions from 'firebase-functions';
-import { Client, isFullPage } from '@notionhq/client';
+import { Client, isFullPage, LogLevel } from '@notionhq/client';
 import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 import { MealIdea } from './model/meal-idea.model';
 import { assertIsNotNullOrUndefined } from './helpers/assert';
 
 const config = functions.config();
+const isEmulated = process.env.FUNCTIONS_EMULATOR === 'true';
 
-export const notionIdeas = functions.region('europe-west1').https.onCall(async (data, context) => {
-	const notion = new Client({
-		auth: config.notion.key,
-	});
+export const notionIdeas = functions
+	.region('europe-west1')
+	.https.onCall(async (data: { databaseId: string }, context) => {
+		const notion = new Client({
+			auth: config.notion.key,
+			logLevel: isEmulated ? LogLevel.DEBUG : LogLevel.WARN,
+		});
 
-	const res = await notion.databases.query({
-		database_id: '9221c1d7570d460f97430a5eddafbaff',
-		filter: {
-			property: 'Nom',
-			rich_text: {
-				is_not_empty: true,
+		const res = await notion.databases.query({
+			database_id: data.databaseId ?? '9221c1d7570d460f97430a5eddafbaff',
+			filter: {
+				property: 'Nom',
+				rich_text: {
+					is_not_empty: true,
+				},
 			},
-		},
-	});
+		});
 
-	const fullPages = res.results.filter(isFullPage);
-	return fullPages.map((page) => extractMealIdeaFromNotionPage(page));
-});
+		const fullPages = res.results.filter(isFullPage);
+		return fullPages.map((page) => extractMealIdeaFromNotionPage(page));
+	});
 
 function extractMealIdeaFromNotionPage(page: PageObjectResponse): MealIdea {
 	const nameProp = page.properties.Nom?.type === 'title' ? page.properties.Nom : null;
@@ -35,11 +39,23 @@ function extractMealIdeaFromNotionPage(page: PageObjectResponse): MealIdea {
 	const tagsProp = page.properties.Tags?.type === 'multi_select' ? page.properties.Tags : null;
 	const tags = tagsProp?.multi_select.map((select) => select.name) ?? [];
 
+	const ratingProp =
+		page.properties['Appréciation']?.type === 'select' ? page.properties['Appréciation'] : null;
+	const rawRating = ratingProp?.select?.name || '';
+	const rating = rawRating.split('👍').length - 1;
+
+	const urlProp = page.properties.URL?.type === 'url' ? page.properties.URL : null;
+	const url = urlProp?.url ?? undefined;
+
 	assertIsNotNullOrUndefined(name);
 
 	return {
+		notionPageUrl: page.url,
+		created_at: page.created_time,
 		name,
 		image,
 		tags,
+		url,
+		rating,
 	};
 }
