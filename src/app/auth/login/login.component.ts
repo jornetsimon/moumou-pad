aa678b8791daa29cda3b75749ff0f2aacb4c4ebd
Simon Jornet <jornetsimon@gmail.com>
2024-08-04 10:58:49 +0200

    remove signInWithRedirect()

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from '@angular/fire/auth';
+import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { HotToastService } from '@ngneat/hot-toast';
 
@@ -19,28 +19,14 @@ export class LoginComponent {
 	) {}
 
 	login() {
-		const navigator: any = window.navigator;
 		const authProvider = new GoogleAuthProvider();
-		if ('standalone' in navigator && navigator.standalone) {
-			// Sign in with redirect on iOS installed PWA
-			signInWithRedirect(this.angularFireAuth, authProvider).then(() => {
-				this.toastService.success(`Connecté`, {
+		signInWithPopup(this.angularFireAuth, authProvider).then((credential) => {
+			if (credential.user) {
+				this.toastService.success(`Connecté en tant que ${credential.user.displayName}`, {
 					duration: 3000,
 				});
 				this.router.navigateByUrl('/');
-			});
-		} else {
-			signInWithPopup(this.angularFireAuth, authProvider).then((credential) => {
-				if (credential.user) {
-					this.toastService.success(
-						`Connecté en tant que ${credential.user.displayName}`,
-						{
-							duration: 3000,
-						}
-					);
-					this.router.navigateByUrl('/');
-				}
-			});
-		}
+			}
+		});
 	}
 }
